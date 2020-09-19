87198f6a39c8359195cbc09727f39e397cfc5c5e
Erik C. Olson <erik@effina.com>
2020-09-19 14:05:37 -0700

    Let Card be as Wide as it Needs

--- resources/views/audits/index.blade.php
+++ resources/views/audits/index.blade.php
@@ -6,45 +6,41 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3"></div>
-        <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
-            <div class="card">
-                <div class="card-header bg-info">
-                    <h1 class="card-title">Urzul Stats</h1>
-                </div>
-                <div class="card-body">
-                    <table id="auditTable" class="table table-active table-bordered">
-                        <thead>
+        <div class="card">
+            <div class="card-header bg-info">
+                <h1 class="card-title">Urzul Stats</h1>
+            </div>
+            <div class="card-body">
+                <table id="auditTable" class="table table-active table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Ran At</th>
+                        <th>Translated From</th>
+                        <th>Translated To</th>
+                        <th>Bot</th>
+                        <th>OS</th>
+                        <th>Browser</th>
+                        <th>Device</th>
+                        <th>File Size</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($audits as $audit)
                         <tr>
-                            <th>Ran At</th>
-                            <th>Translated From</th>
-                            <th>Translated To</th>
-                            <th>Bot</th>
-                            <th>OS</th>
-                            <th>Browser</th>
-                            <th>Device</th>
-                            <th>File Size</th>
+                            <td>{{ $audit->created_at->format('Y-m-d H:i:s') }}</td>
+                            <td>{{ $audit->source_language ?? '—' }}</td>
+                            <td>{{ $audit->destination_language ?? '—' }}</td>
+                            <td>{{ $audit->bot ? 'Yes' : 'No' }}</td>
+                            <td>{{ $audit->os ?? '—' }}</td>
+                            <td>{{ $audit->browser ?? '—' }}</td>
+                            <td>{{ $audit->device ?? '—' }}</td>
+                            <td>{{ $audit->file_size ?? '—' }}</td>
                         </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($audits as $audit)
-                            <tr>
-                                <td>{{ $audit->created_at->format('Y-m-d H:i:s') }}</td>
-                                <td>{{ $audit->source_language ?? '—' }}</td>
-                                <td>{{ $audit->destination_language ?? '—' }}</td>
-                                <td>{{ $audit->bot ? 'Yes' : 'No' }}</td>
-                                <td>{{ $audit->os ?? '—' }}</td>
-                                <td>{{ $audit->browser ?? '—' }}</td>
-                                <td>{{ $audit->device ?? '—' }}</td>
-                                <td>{{ $audit->file_size ?? '—' }}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
-        <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3"></div>
     </div>
 @endsection
 
